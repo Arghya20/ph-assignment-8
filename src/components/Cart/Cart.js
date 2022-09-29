32ff046dac8773bc1aed1eeb4fc5e9c8c97f57fd
Arghya20 <arghyaghosh.vlog@gmail.com>
2022-09-29 19:41:30 +0530

    cart section desine done

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,99 @@
+import React from "react";
+import "./user.jpg";
+
+const Cart = () => {
+  return (
+    <div className=" lg:sticky top-4">
+      {/*======= User Section ======== */}
+      <div className="flex justify-evenly items-center mt-4 p-2 m-3 rounded-md mx-auto">
+        <img
+          className="w-1/3 rounded-full shadow-lg"
+          src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+
+        <div>
+          <h2 className="text-lg font-bold">Alexander</h2>
+          <div className="flex ">
+            <p>Ontario,</p>
+            <p>Canada</p>
+          </div>
+        </div>
+      </div>
+
+      {/*========= user Information Section==========  */}
+      <div className="flex justify-between p-3 items-center bg-blue-50 m-6 rounded-md ">
+        <div>
+          <p className="text-center">
+            <strong>65</strong> kg
+          </p>
+          <h4>Weight</h4>
+        </div>
+        <div>
+          <p className="text-center">
+            <strong>5.6</strong>
+          </p>
+          <h4>Height</h4>
+        </div>
+        <div>
+          <p className="text-center">
+            <strong>35</strong>
+          </p>
+          <h4>Age</h4>
+        </div>
+      </div>
+
+      {/* ====Break Section ======== */}
+      <h3 className="m-7 font-bold mt-14 -mb-4 ">Add A Break</h3>
+      <div className="flex justify-between p-3 items-center bg-blue-50 m-5 rounded-md ">
+        <div className="bg-white rounded-full p-2 hover:bg-blue-300 cursor-pointer">
+          <p className="text-center">
+            <strong>10</strong>s
+          </p>
+        </div>
+        <div className="bg-white rounded-full p-2 hover:bg-blue-300 cursor-pointer">
+          <p className="text-center">
+            <strong>15</strong>s
+          </p>
+        </div>
+        <div className="bg-white rounded-full p-2 hover:bg-blue-300 cursor-pointer">
+          <p className="text-center">
+            <strong>20s</strong>
+          </p>
+        </div>
+        <div className="bg-white rounded-full p-2 hover:bg-blue-300 cursor-pointer">
+          <p className="text-center">
+            <strong>30</strong>s
+          </p>
+        </div>
+        <div className="bg-white rounded-full p-2 hover:bg-blue-300 cursor-pointer">
+          <p className="text-center">
+            <strong>45</strong>s
+          </p>
+        </div>
+      </div>
+
+      {/*========= Exercise Details =========== */}
+      <h2 className="mx-5 mb-2 font-bold mt-8">Exercise Details</h2>
+      <div className="flex justify-evenly bg-blue-50 mx-4 p-4 rounded-lg">
+        <p>Exercise Time</p>
+        <p className="text-gray-400">
+          <span>100</span> Seconds
+        </p>
+      </div>
+    
+      <div className="flex justify-evenly bg-blue-50 mx-4 p-4 rounded-lg mt-4">
+        <p>Break time</p>
+        <p className="text-gray-400">
+          <span>100</span> Seconds
+        </p>
+      </div>
+
+      {/*==== Activity Complited Button ======= */}
+      <button >Activity Completed</button>
+
+    </div>
+  );
+};
+
+export default Cart;
--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
+import Product from "../Product/Product";
 
 const Display = () => {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
   return (
     <div>
-      
+      <div className=" mx-auto bg-blue-50 px-3 grid grid-cols-[1fr] md:grid-cols-[4fr,1fr]">
+        <div>
+          <h1 className="text-3xl text-center font-bold py-4 bg-blue-50">
+            Burn ToDay
+          </h1>
+          <h3 className="text-xl text-center py-3 bg-blue-50">
+            Select your today’s exercise
+          </h3>
+          <div className="main-section grid grid-cols-1 md:grid-cols-3 p-4">
+            {user.map((usr) => (
+              <Product usr={usr}></Product>
+            ))}
+          </div>
+        </div>
+
+        <div className="bg-white">
+          <Cart></Cart>
+          <img src="" alt="" />
+        </div>
+      </div>
+
     </div>
   );
 };
